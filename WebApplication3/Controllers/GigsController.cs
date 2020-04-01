cd3b8b35f3e19a2ff1815e3266d1acc5f1f2a688
shuvo <shuvo@DESKTOP-159F2AF>
2020-04-01 23:31:32 +0600

    Create Add a Gig Form.

---
+++ WebApplication3/Controllers/GigsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using WebApplication3.Models;
+using WebApplication3.ViewModels;
+
+namespace WebApplication3.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var ViewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(ViewModel);
+        }
+    }
+}
