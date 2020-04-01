14eb8770fb7ef4577e32e130b2cd24793a817ac7
shuvo <shuvo@DESKTOP-159F2AF>
2020-04-01 21:26:11 +0600

    Polulate Genres Table

---
+++ WebApplication3/Models/Gig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Venue { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+    }
+}
