95e29b7ef484aff949c6de2d51dd891dbd53eb61
shuvo <shuvo@DESKTOP-159F2AF>
2020-04-02 16:31:06 +0600

    Move Date Time Calculation To GIgform Viewmodel

--- WebApplication3/Controllers/GigsController.cs
+++ WebApplication3/Controllers/GigsController.cs
@@ -33,7 +33,7 @@ namespace WebApplication3.Controllers
             var gig = new Gig
             {
                 ArtistId = User.Identity.GetUserId(),
-                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                DateTime = viewModel.DateTime,
                 GenreId = viewModel.Genre,
                 Venue = viewModel.Venue
 
